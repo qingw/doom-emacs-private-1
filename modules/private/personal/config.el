7c9bac85618c6e1682d1cbf036461119206ddd08
driftcrow <driftcrow@gmail.com>
2018-02-24 22:02:13 +0800

    remove engine-mode, use +lookup

--- modules/private/personal/config.el
+++ modules/private/personal/config.el
@@ -295,62 +295,26 @@ Enable completion of info from magithub in the current buffer.
         :n "u" #'pipenv-uninstall
         ))
 
-(def-package! engine-mode
-    :commands (defengine )
-    :defines search-engine-alist
-    :init
-    (progn
-      (setq search-engine-alist
-            '((amazon
-               :name "Amazon"
-               :url "http://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%%3Daps&field-keywords=%s")
-              (bing
-               :name "Bing"
-               :url "http://www.bing.com/search?q=%s")
-              (duck-duck-go
-               :name "Duck Duck Go"
-               :url "https://duckduckgo.com/?q=%s")
-              (google
-               :name "Google"
-               :url "http://www.google.com/search?ie=utf-8&oe=utf-8&q=%s")
-              (google-images
-               :name "Google Images"
-               :url "http://www.google.com/images?hl=en&source=hp&biw=1440&bih=795&gbv=2&aq=f&aqi=&aql=&oq=&q=%s")
-              (github
-               :name "Github"
-               :url "https://github.com/search?ref=simplesearch&q=%s")
-              (google-maps
-               :name "Google Maps"
-               :url "http://maps.google.com/maps?q=%s")
-              (twitter
-               :name "Twitter"
-               :url "https://twitter.com/search?q=%s")
-              (project-gutenberg
-               :name "Project Gutenberg"
-               :url "http://www.gutenberg.org/ebooks/search.html/?format=html&default_prefix=all&sort_order=&query=%s")
-              (youtube
-               :name "YouTube"
-               :url "http://www.youtube.com/results?aq=f&oq=&search_query=%s")
-              (stack-overflow
-               :name "Stack Overflow"
-               :url "https://stackoverflow.com/search?q=%s")
-              (wikipedia
-               :name "Wikipedia"
-               :url "http://www.wikipedia.org/search-redirect.php?language=en&go=Go&search=%s")
-              (rfcs
-               :name "RFC"
-               :url "http://pretty-rfc.herokuapp.com/search?q=%s")
-              (wolfram-alpha
-               :name "Wolfram Alpha"
-               :url "http://www.wolframalpha.com/input/?i=%s")))
-      (dolist (engine search-engine-alist)
-        (let ((func (intern (format "engine/search-%S" (car engine)))))
-          (autoload func "engine-mode" nil 'interactive))))
-    :config
-    (progn
-      (engine-mode t)
-      (engine/set-keymap-prefix (kbd "s-s"))
-      (dolist (engine search-engine-alist)
-        (let* ((cur-engine (car engine))
-               (engine-url (plist-get (cdr engine) :url)))
-          (eval `(defengine ,cur-engine ,engine-url))))))
+;; search online , has no efficient instrument change variable in autoload module
+(setq +lookup-provider-url-alist
+      '(("Google"             . "https://google.com/search?q=%s")
+        ("Google images"      . "https://google.com/images?q=%s")
+        ("Google maps"        . "https://maps.google.com/maps?q=%s")
+        ("Project Gutenberg"  . "http://www.gutenberg.org/ebooks/search/?query=%s")
+        ("DuckDuckGo"         . "https://duckduckgo.com/?q=%s")
+        ("DevDocs.io"         . "https://devdocs.io/#q=%s")
+        ("StackOverflow"      . "https://stackoverflow.com/search?q=%s")
+        ("Github"             . "https://github.com/search?ref=simplesearch&q=%s")
+        ("Youtube"            . "https://youtube.com/results?aq=f&oq=&search_query=%s")
+        ("Wolfram alpha"      . "https://wolframalpha.com/input/?i=%s")
+        ("Wikipedia"          . "https://wikipedia.org/search-redirect.php?language=en&go=Go&search=%s")
+        ("Zhihu"              . "https://www.zhihu.com/search?q=%s&type=content")
+        ("Baidu"              . "http://www.baidu.com/s?wd=%s")
+        ("Pinterest"          . "https://www.pinterest.com/search/pins/?q=%s")
+        ("Amazon"             . "http://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%%3Daps&field-keywords=%s")
+        ("Douban"             . "https://www.douban.com/search?q=%s")
+        ("Bing"               . "http://www.bing.com/search?q=%s")
+        ("Twitter"            . "https://twitter.com/search?q=%s")
+        ("Weibo"              . "http://s.weibo.com/weibo/%s")
+        ("RFC"                . "http://pretty-rfc.herokuapp.com/search?q=%s")
+        ))
--- modules/private/personal/packages.el
+++ modules/private/personal/packages.el
@@ -12,7 +12,6 @@
 (package! whitespace-cleanup-mode)
 
 (package! atomic-chrome)
-(package! engine-mode)
 
 (package! prog-fill)
 

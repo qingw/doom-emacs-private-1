afc48c476bba357b5fc6640b5f92c7557a74d19e
driftcrow <driftcrow@gmail.com>
2018-05-25 12:01:08 +0800

    update init from example

--- init.el
+++ init.el
@@ -30,20 +30,17 @@
 ;; (require 'core (concat user-emacs-directory "core/core"))
 
 (doom! :feature
-       (popup            ; tame sudden yet inevitable temporary windows
-        +all             ; catch all popups that start with an asterix
-        +defaults)       ; default popup rules
-       debugger          ; FIXME stepping through code, to help you add bugs
+      ;debugger          ; FIXME stepping through code, to help you add bugs
        eval              ; run code, run (also, repls)
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        (lookup           ; helps you navigate your code and documentation
         +devdocs         ; ...on devdocs.io online
         +docsets)        ; ...or in Dash docsets locally
-       services          ; TODO managing external services & code builders
        snippets          ; my elves. They type so I don't have to
        spellcheck        ; tasing you for misspelling mispelling
-       syntax-checker    ; tasing you for every semicolon you forget
+       (syntax-checker   ; tasing you for every semicolon you forget
+        +childframe)     ; use childframes for error popups (Emacs 26+ only)
        version-control   ; remember, remember that commit in November
        workspaces        ; tab emulation, persistence & separate workspaces
 
@@ -64,6 +61,10 @@
        evil-goggles      ; display visual hints when editing in evil
        hl-todo           ; highlight TODO/FIXME/NOTE tags
        nav-flash         ; blink the current line after jumping
+       neotree           ; a project drawer, like NERDTree for vim
+       (popup            ; tame sudden yet inevitable temporary windows
+        +all             ; catch all popups that start with an asterix
+        +defaults)       ; default popup rules
       ;tabbar            ; FIXME an (incomplete) tab bar for Emacs
       ;unicode           ; extended unicode support for various languages
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
@@ -72,18 +73,17 @@
        :tools
        dired             ; making dired pretty [functional]
        editorconfig      ; let someone else argue about tabs vs spaces
-       ein               ; tame Jupyter notebooks with emacs
        electric-indent   ; smarter, keyword-based electric-indent
+      ;ein               ; tame Jupyter notebooks with emacs
        eshell            ; a consistent, cross-platform shell (WIP)
        gist              ; interacting with github gists
        imenu             ; an imenu sidebar and searchable code index
-       impatient-mode    ; show off code over HTTP
        macos             ; MacOS-specific commands
        make              ; run make tasks from Emacs
        magit             ;
-       neotree           ; a project drawer, like NERDTree for vim
        ;password-store    ; password manager for nerds
        pdf               ; pdf enhancements
+      ;prodigy           ; FIXME managing external services & code builders
       ;rgb               ; creating color strings
        rotate-text       ; cycle region at point between text candidates
        term              ; terminals in Emacs
@@ -112,16 +112,16 @@
        ledger            ; an accounting system in Emacs
        lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
-       ;; nix               ; I hereby declare "nix geht mehr!"
-       ;; ocaml             ; an objective camel
+      ;nim               ; python + lisp at the speed of c
+      ;nix               ; I hereby declare "nix geht mehr!"
+      ;ocaml             ; an objective camel
        (org              ; organize your plain life in plain text
         +attach          ; custom attachment system
         +babel           ; running code in org
         +capture         ; org-capture in and outside of Emacs
         +export          ; Exporting org to whatever you want
-        +present         ; Emacs for presentations
-        +publish)        ; Emacs+Org as a static site generator
-       ;; perl              ; write code no one else can comprehend
+        +present)        ; Emacs for presentations
+      ;perl              ; write code no one else can comprehend
        ;php               ; perl's insecure younger brother
        plantuml          ; diagrams for confusing people more
        ;purescript        ; javascript, but functional
@@ -132,8 +132,7 @@
        ;scala             ; java, but good
        sh                ; she sells (ba|z)sh shells on the C xor
        ;swift             ; who asked for emoji variables?
-       ;typescript        ; javascript, but better
-       web               ; the tubes
+      ;web               ; the tubes
 
        ;; Applications are complex and opinionated modules that transform Emacs
        ;; toward a specific purpose. They may have additional dependencies and
@@ -143,9 +142,12 @@
       irc               ; how neckbeards socialize
       ;(rss +org)        ; emacs as an RSS reader
       ;twitter           ; twitter client https://twitter.com/vnought
-       (write            ; emacs as a word processor (latex + org + markdown)
-        +wordnut         ; wordnet (wn) search
-        +langtool)       ; a proofreader (grammar/style check) for Emacs
+      (write            ; emacs as a word processor (latex + org + markdown)
+      +wordnut         ; wordnet (wn) search
+      +langtool)       ; a proofreader (grammar/style check) for Emacs
+
+       :collab
+      ;impatient-mode    ; show off code over HTTP
 
        :config
        ;; The default module set reasonable defaults for Emacs. It also provides
